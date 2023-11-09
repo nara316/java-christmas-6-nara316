c032c8b0d7c8e9566861e6b6aa5e0639f4e76e10
Nara Park <skfk6352@naver.com>
2023-11-10 05:34:27 +0900

    <feat>(OutView): 각 결과 출력을 안내하는 메서드 구현

---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,49 @@
+package christmas.view;
+
+public class OutputView {
+
+    private static final String DISCOUNT_PREVIEW_MESSAGE = "12월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!\n";
+    private static final String ORDER_MENU_MESSAGE = "<주문 메뉴>";
+    private static final String TOTAL_ORDER_PRICE_MESSAGE = "<할인 전 총주문 금액>\n" + "%,d원";
+    private static final String GIFT_MENU_MESSAGE = "<증정 메뉴>\n";
+    private static final String BENEFITS_DETAILS_MESSAGE = "<혜택 내역>\n";
+    private static final String TOTAL_BENEFIT_PRICE_MESSAGE = "<총혜택 금액>\n" + "%,d원";
+    private static final String TOTAL_ORDER_PRICE_AFTER_BENEFIT_MESSAGE = "<할인 후 예상 결제 금액>\n" + "%,d원";
+    private static final String EVENT_BADGE_MESSAGE = "<12월 이벤트 배지>\n";
+
+    public void printTotalResult() {
+        printDiscountPreview();
+    }
+
+    private void printDiscountPreview() {
+        System.out.println(DISCOUNT_PREVIEW_MESSAGE);
+    }
+
+    private void printOrderMenu() {
+        System.out.println(ORDER_MENU_MESSAGE);
+    }
+
+    private void printTotalOrderPrice() {
+        System.out.println(TOTAL_ORDER_PRICE_MESSAGE);
+    }
+
+    private void printGiftMenu() {
+        System.out.println(GIFT_MENU_MESSAGE);
+    }
+
+    private void printBenefitsDetails() {
+        System.out.println(BENEFITS_DETAILS_MESSAGE);
+    }
+
+    private void printTotalBenefitPrice() {
+        System.out.println(TOTAL_BENEFIT_PRICE_MESSAGE);
+    }
+
+    private void printTotalOrderPriceAfterBenefit() {
+        System.out.println(TOTAL_ORDER_PRICE_AFTER_BENEFIT_MESSAGE);
+    }
+
+    private void EVENT_BADGE_MESSAGE() {
+        System.out.println(EVENT_BADGE_MESSAGE);
+    }
+}
