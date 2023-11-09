e2e25194330c521629fbc28a317ecbc8db8b0446
Nara Park <skfk6352@naver.com>
2023-11-10 02:37:09 +0900

    <feat>(InputView): 사용자에게 입력값을 요청하는 메서드 구현

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,27 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+public class InputView {
+
+    private static final String REQUEST_VISIT_DATE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n"
+            + "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    private static final String REQUEST_MENU_AND_AMOUNT = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. "
+            + "(e.g. 해산물파스타-2,레드와인-1,초코케이스-1)";
+
+    public String inputVisitDate() {
+        System.out.println(REQUEST_VISIT_DATE);
+        String visitDate = getUserInput();
+        return visitDate;
+    }
+
+    public String inputMenuAndAmount() {
+        System.out.println(REQUEST_MENU_AND_AMOUNT);
+        String menuAndAmount = getUserInput();
+        return menuAndAmount;
+    }
+
+    private String getUserInput() {
+        return Console.readLine();
+    }
+}
