726a23af9be9b47f595502512ad98fbc2cc35bda
Nara Park <skfk6352@naver.com>
2023-11-10 04:30:21 +0900

    <test>(VisitDate): 날짜 범위 검증 메서드에 대한 단위 테스트 메서드 구현

--- src/main/java/christmas/constant/ExceptionConstant.java
+++ src/main/java/christmas/constant/ExceptionConstant.java
@@ -5,7 +5,7 @@ public enum ExceptionConstant {
     ERROR_TITLE("[ERROR] "),
     INPUT_IS_ESSENTIAL("입력값은 필수입니다."),
     INPUT_IS_NUMBER("입력값은 정수만을 허용합니다."),
-    VISIT_DATE_RANGE("유효해지 않은 날짜입니다. 다시 입력해 주세요.");
+    VISIT_DATE_RANGE("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
 
     private final String message;
 
---
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -0,0 +1,35 @@
+package christmas.domain;
+
+import static christmas.constant.ExceptionConstant.VISIT_DATE_RANGE;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class VisitDateTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -999})
+    void 방문날짜가_1_미만일_경우_예외발생(int date) {
+        assertThatThrownBy(() -> VisitDate.of(date))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(VISIT_DATE_RANGE.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {32, 35, 888})
+    void 방문날짜가_31_초과할_경우_예외발생(int date) {
+        assertThatThrownBy(() -> VisitDate.of(date))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(VISIT_DATE_RANGE.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 16, 31})
+    void 값이_유효하면_예외가_발생하지_않음(int date) {
+        assertThatCode(() -> VisitDate.of(date))
+                .doesNotThrowAnyException();
+    }
+}
