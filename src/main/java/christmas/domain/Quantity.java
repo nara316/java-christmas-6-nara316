ff90940aa0fbf8e4708934d2b342dee757c1e514
Nara Park <skfk6352@naver.com>
2023-11-10 19:46:52 +0900

    <feat>(Quantity): 각 주문수량의 최소값을 검증하는 메서드 구현

--- src/main/java/christmas/domain/Quantity.java
+++ src/main/java/christmas/domain/Quantity.java
@@ -1,5 +1,9 @@
 package christmas.domain;
 
+import static christmas.constant.NumberConstant.ORDER_QUANTITY_MIN;
+
+import christmas.constant.ExceptionConstant;
+
 public class Quantity {
 
     private final int value;
@@ -10,5 +14,11 @@ public class Quantity {
 
     public static Quantity from(int value) {
         return new Quantity(value);
+    }
+
+    private void validateMinValue() {
+        if (value < ORDER_QUANTITY_MIN.getNumber()) {
+            throw new IllegalArgumentException(ExceptionConstant.WRONG_ORDER.getMessage());
+        }
     }
 }
