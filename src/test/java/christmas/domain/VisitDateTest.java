82f378f73c3363fdbac3b437d53db10043c48f8e
Nara Park <skfk6352@naver.com>
2023-11-14 02:45:07 +0900

    <test>(VisitDate): 방문날짜에 대한 입력값이 정수인지 검증하는 유효성 테스트 구현

--- src/test/java/christmas/domain/VisitDateTest.java
+++ src/test/java/christmas/domain/VisitDateTest.java
@@ -11,25 +11,33 @@ import org.junit.jupiter.params.provider.ValueSource;
 class VisitDateTest {
 
     @ParameterizedTest
-    @ValueSource(ints = {0, -1, -999})
-    void 방문날짜가_1_미만일_경우_예외발생(int date) {
-        assertThatThrownBy(() -> VisitDate.from(date))
+    @ValueSource(strings = {"a", "삼일", "1일"})
+    void 방문날짜가_정수가_아닌경우_예외발생(String userInput) {
+        assertThatThrownBy(() -> VisitDate.from(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(VISIT_DATE_RANGE.getMessage());
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {32, 35, 888})
-    void 방문날짜가_31_초과할_경우_예외발생(int date) {
-        assertThatThrownBy(() -> VisitDate.from(date))
+    @ValueSource(strings = {"0", "-1", "-999"})
+    void 방문날짜가_1_미만일_경우_예외발생(String userInput) {
+        assertThatThrownBy(() -> VisitDate.from(userInput))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(VISIT_DATE_RANGE.getMessage());
     }
 
     @ParameterizedTest
-    @ValueSource(ints = {1, 16, 31})
-    void 값이_유효하면_예외가_발생하지_않음(int date) {
-        assertThatCode(() -> VisitDate.from(date))
+    @ValueSource(strings = {"32", "35", "888"})
+    void 방문날짜가_31_초과할_경우_예외발생(String userInput) {
+        assertThatThrownBy(() -> VisitDate.from(userInput))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining(VISIT_DATE_RANGE.getMessage());
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "16", "31"})
+    void 값이_유효하면_예외가_발생하지_않음(String userInput) {
+        assertThatCode(() -> VisitDate.from(userInput))
                 .doesNotThrowAnyException();
     }
 }
